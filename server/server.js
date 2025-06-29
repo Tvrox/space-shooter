fc68c079c2a7d1965d9448105e80ae6604521f7e
Tvrox <yegoriyy@gmail.com>
2025-06-29 16:52:16 +0300

    Switched to .env-based Firebase config

--- server/server.js
+++ server/server.js
@@ -23,14 +23,14 @@ const serviceAccount = {
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
   token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
 };
 
+// Инициализация Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.database();
