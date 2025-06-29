63c6941810699c777bb04872190a9372c3ddac09
Tvrox <yegoriyy@gmail.com>
2025-06-29 19:32:44 +0300

    Updated the configuration server.js to work with .env

--- server/server.js
+++ server/server.js
@@ -15,17 +15,17 @@ app.use(express.json());
 
 // 🔑 Подключаем ключ Firebase
 const adminConfig = {
-  type: 'service_account',
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
-  universe_domain: process.env.FB_UNIVERSE_DOMAIN
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
 };
 
 admin.initializeApp({
