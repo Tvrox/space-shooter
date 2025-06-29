617d944b0a494febc5c888a11dc7af4770a6e49d
Tvrox <yegoriyy@gmail.com>
2025-06-29 18:49:13 +0300

    We switched to Firebase via .env

--- server/server.js
+++ server/server.js
@@ -14,11 +14,22 @@ app.use(cors());
 app.use(express.json());
 
 // 🔑 Подключаем ключ Firebase
-const serviceAccount = require('./firebase-key.json');
-
+const adminConfig = {
+  type: 'service_account',
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+  universe_domain: process.env.FB_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(adminConfig),
   databaseURL: 'https://space-shooter-1e24a-default-rtdb.europe-west1.firebasedatabase.app/'
 });
 
